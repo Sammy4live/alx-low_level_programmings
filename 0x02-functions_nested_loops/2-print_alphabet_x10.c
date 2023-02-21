00bca83cf66c741e8e8338261148343111d8b554
Samson <samuelohida2022@gmail.com>
2023-02-21 06:27:25 -0800

    2-print_alphabet_x10.c

--- 0x02-functions_nested_loops/2-print_alphabet_x10.c
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -11,13 +11,13 @@ void print_alphabet_x10(void)
 	char c;
 	int i = 0;
 
-	while (i <=0)
+	while (i <= 0)
 	{
-		for (c = 'a'; c <= 'z'; c++)
+	for (c = 'a'; c <= 'z'; c++)
 		{
-			_putchar(c);
+		_putchar(c);
 		}
-			_putchar('\n');
-			i++;
+		_putchar('\n');
+		i++;
 		}
-	}
+}
