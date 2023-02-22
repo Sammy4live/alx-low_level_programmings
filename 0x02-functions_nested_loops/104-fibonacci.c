780fb420884109e7feaf627ad08256b9e6280a56
Samson <samuelohida2022@gmail.com>
2023-02-22 11:36:50 -0800

    104-fibonacci.c

--- 0x02-functions_nested_loops/104-fibonacci.c
+++ 0x02-functions_nested_loops/104-fibonacci.c
@@ -7,7 +7,6 @@
  */
 
 int main(void)
-
 {
 	int count;
 	unsigned long fib1 = 0, fib2 = 1, sum;
@@ -16,6 +15,7 @@ int main(void)
 
 	for (count = 0; count < 92; count++)
 	{
+	
 		sum = fib1 + fib2;
 	printf("%lu, ", sum);
 	fib1 = fib2;
@@ -25,16 +25,19 @@ int main(void)
 	fib2_half1 = fib2 / 10000000000;
 	fib1_half2 = fib1 % 10000000000;
 	fib2_half2 = fib2 % 10000000000;
+	
 	for (count = 93; count < 99; count++)
 	{
 	half1 = fib1_half1 + fib2_half1;
 	half2 = fib1_half2 + fib2_half2;
+	
 	if (fib1_half2 + fib2_half2 > 9999999999)
 	{
-		half1 += 1;
-		half2 %= 1000000000;
+	half1 += 1;
+	half2 %= 1000000000;
 	}
 	printf("%lu%lu", half1, half2);
+	
 	if (count != 98)
 	print(", ");
 	fib1_half1 = fib2_half1;
@@ -43,5 +46,6 @@ int main(void)
 	fib2_half2 = half2;
 	}
 	printf("\n");
+	
 	return (0);
 }
