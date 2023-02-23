cd838c961b147851d9ad38b71cff08cd4670b403
Samson <samuelohida2022@gmail.com>
2023-02-23 07:33:13 -0800

    2-largest_number.c git commit -m 2-largest_number.c
    clear
    
    git commit -m 2-largest_number.c

--- 0x03-debugging/2-largest_number.c
+++ 0x03-debugging/2-largest_number.c
@@ -9,13 +9,14 @@
  */
 
 int largest_number(int a, int b, int c)
+
 {
 	int largest;
 	if (a >= b && a >= c)
 	{
 	largest = a;
 	}
-	else if (b >= a && a >= c)
+	else if (b >= a && b >= c)
 	{
 	largest = b;
 	}
@@ -23,5 +24,6 @@ int largest_number(int a, int b, int c)
 	{
 	largest = c;
 	}
+
 	return (largest);
 }
